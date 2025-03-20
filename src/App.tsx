2362a0a7ba89ae1fbf915f8e572145adb6f531b2
Ellyssa Cruz <ellyssagpcruz@gmail.com>
2025-03-21 02:29:55 +0800

    Removed unnecessary import on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
